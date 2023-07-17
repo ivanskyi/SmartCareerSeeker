90b5c8e50d582ca8cecfd29a1f639547456f3fc7
Oleh <olehfromukraine@gmail.com>
2023-07-17 13:39:35 +0300

    Created project

---
+++ src/test/java/com/example/smartcareerseeker/SmartCareerSeekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartcareerseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCareerSeekerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
